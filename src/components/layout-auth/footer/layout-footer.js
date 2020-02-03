b97ec1286f2724d2b99d2a0a35b7873b79f76409
w-sr <worldsr.up@gmail.com>
2020-02-03 21:00:28 +0200

    first commit

---
+++ src/components/layout-auth/footer/layout-footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Components
+import Container from './styles';
+
+export default function LayoutFooter() {
+  return (
+    <Container>
+      <span>
+        <a href="https://github.com/lirbaj">Github</a>
+      </span>
+    </Container>
+  );
+}
